13e7480502217524418567d4eaa478a818bd2c08
zhoulf <zhouliangfei@shentie.com.cn>
2022-05-05 15:25:43 +0800

    更换logo

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -2,10 +2,7 @@
   <div class="navbar">
     <div class="left-side" :style="{ width: menuWidth + 'px' }">
       <a-space>
-        <img
-          alt="logo"
-          src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image"
-        />
+        <img alt="logo" src="../../assets/logo.svg" />
         <a-typography-title
           :style="{ margin: 0, fontSize: '18px' }"
           :heading="5"
